9beb015811019ebf5ddbf551445205b3b85fd41d
TheGreatBeanLord <thompsonjoeljames@gmail.com>
2021-12-13 13:24:06 -0700

    f

---
+++ OOP exersizes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OOP_exersizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Backpack smallBlue = new Backpack("blue", "small", null, false);
+            Backpack mediumRed = new Backpack("red", "medium", null, false);
+            Backpack largeGreen = new Backpack("red", "medium", null, false);
+
+            smallBlue.openBag();
+            smallBlue.putIn("lunch");
+            smallBlue.putIn("jacket");
+            smallBlue.closeBag();
+            smallBlue.openBag();
+            Console.WriteLine(smallBlue.IsOpen);
+            smallBlue.takeOut("jacket");
+            smallBlue.closeBag();
+
+
+        }
+    }
+}
